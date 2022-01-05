ac4a1fb4397c14130c76098c509037efa348791f
Metin <metinmayi@gmail.com>
2022-01-05 19:46:26 +0100

    Added CORS

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import express from "express";
+import cors from "cors";
 
 import usersRoutes from "./routes/users.js";
 
@@ -6,6 +7,7 @@ const app = express();
 const PORT = process.env.PORT;
 
 app.use(express.json());
+app.use(cors({ methods: ["GET"] }));
 
 app.use("/users", usersRoutes);
 
