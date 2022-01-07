ab9eebdf467cea9c1c4d8f95b61e99a832c44ecb
Metin <metinmayi@gmail.com>
2022-01-07 19:46:05 +0100

    Added POST as a method

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const app = express();
 const PORT = process.env.PORT;
 
 app.use(express.json());
-app.use(cors({ methods: ["GET"] }));
+app.use(cors({ methods: ["GET", "POST"] }));
 
 app.use("/users", usersRoutes);
 
