35a0d548e22b37bd7132e2d743fef002951eb9b4
Metin <metinmayi@gmail.com>
2022-02-03 20:41:43 +0100

    Domain: .herokuapp.com

--- index.js
+++ index.js
@@ -70,7 +70,7 @@ app.post("/login", async (req, res) => {
 		maxAge: 60 * 60 * 1000,
 		sameSite: "none",
 		secure: true,
-		domain: ".heroku.com",
+		domain: ".herokuapp.com",
 	});
 	res.json("Login successful");
 });
@@ -84,7 +84,7 @@ app.get("/logout", async (req, res) => {
 			maxAge: 1,
 			sameSite: "None",
 			secure: true,
-			domain: ".heroku.com",
+			domain: ".herokuapp.com",
 		})
 		.redirect("/");
 });
