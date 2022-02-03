626b5b3d72558c09eaff835fb48bb8d98815c103
Metin <metinmayi@gmail.com>
2022-02-03 19:51:58 +0100

    samesite:none

--- index.js
+++ index.js
@@ -68,7 +68,7 @@ app.post("/login", async (req, res) => {
 	res.cookie("jwt", token, {
 		httpOnly: true,
 		maxAge: 60 * 60 * 1000,
-		sameSite: "lax",
+		sameSite: "none",
 		secure: true,
 	});
 	res.json("Login successful");
